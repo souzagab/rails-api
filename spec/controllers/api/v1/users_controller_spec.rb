096ec23a52fc036ec3ac46f2ac99f30c0cdda97b
Gabriel Souza <desouza.gabriel@icloud.com>
2019-09-30 13:55:07 -0300

    Add update with invalid email test on user controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -14,9 +14,17 @@ RSpec.describe Api::V1::UsersController, type: :request do
             patch api_v1_user_path(User.first.id), params:{ user: attributes_for(:user) } 
             expect(response).to have_http_status(200)
         end
+        it 'Update user with invalid email' do
+            patch api_v1_user_path(User.first.id), params:{ user: {email:'invalidemail.com'} } 
+            expect(response).to_not have_http_status(200)
+        end
         it 'Delete user' do
             delete api_v1_user_path(User.first.id)
             expect(response).to have_http_status(204)
+        end
+        it 'Show user' do
+            get api_v1_user_path(User.first.id)
+            expect(response).to have_http_status(200)
         end
     end
     
