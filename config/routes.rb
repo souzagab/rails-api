4a08300a407e5c6de0019ac4e34688f07243fe65
Gabriel S <thesouzagab@gmail.com>
2023-05-11 19:51:44 -0300

    style: remove bang from methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Healthcheck
   get "healthz" => "rails/health#show"
 
-  match "*path", to: "application#route_not_found!", via: :all
+  match "*path", to: "application#route_not_found", via: :all
 end
